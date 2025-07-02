5b8e24e7dc2251f9d5179a5a01878b4bc68b284c
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2025-07-02 22:38:47 +0530

    Create set .union operation.py

---
+++ HackerRank /set .union operation.py
@@ -0,0 +1,19 @@
+n1 = int(input())
+s1 = set(map(int,(input().split())))
+n2 = int(input())
+s2 = set(map(int,(input().split())))
+s3 = s1.union(s2)
+print(len(s3))
+'''
+input
+
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+
+output
+
+13
+
+'''
