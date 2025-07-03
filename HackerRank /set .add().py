989930c9c86531488267e899186fd446a7d06013
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2025-07-03 12:52:11 +0530

    Create set .add().py

---
+++ HackerRank /set .add().py
@@ -0,0 +1,25 @@
+n = int(input())
+s = set()
+for i in range(0,n):
+    n = input()
+    s.add(n)
+print(len(s))
+
+'''
+
+input
+
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France
+
+output
+
+5
+
+'''
