db5386e064f0d703acb81b4981e5ea771ba6de7b
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2025-07-15 10:56:55 +0530

    Update check subsets.py

--- HackerRank /check subsets.py
+++ HackerRank /check subsets.py
@@ -8,6 +8,27 @@ for i in range (t):
         print("False")
     else:
         print("True")
+'''
+input
+3
+5
+1 2 3 5 6
+9
+9 8 5 6 3 2 1 4 7
+1
+2
+5
+3 6 5 4 1
+7
+1 2 3 5 6 8 9
+3
+9 8 2
 
+output
+
+True
+False
+False
+''''
 
 
