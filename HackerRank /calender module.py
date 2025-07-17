7c06752f259730324f541681148fcc476b780c71
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2025-07-17 22:54:46 +0530

    Update calender module.py

--- HackerRank /calender module.py
+++ HackerRank /calender module.py
@@ -4,3 +4,11 @@ mon,day,year = map(int,input().split())
 daynr = calendar.weekday(year,mon,day)
 days = ['MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY']
 print(days[daynr])
+
+'''
+input
+08 05 2015
+
+ouput
+
+MONDAY
