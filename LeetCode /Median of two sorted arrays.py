083c086183afe998734e96bc6b499270c50b258f
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2025-07-23 12:56:57 +0530

    Create Median of two sorted arrays.py

---
+++ LeetCode /Median of two sorted arrays.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+        n = len(nums1)
+        if n%2 == 0:
+            med = (nums1[n//2-1] + nums1[n//2]) / 2
+            return med
+        else:
+            med = nums1[n//2]
+            return med
+
+'''
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+'''
