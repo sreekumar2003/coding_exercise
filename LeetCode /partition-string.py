6b11ab8a747f79589afafc0d22a8a99f94b270fd
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2026-02-24 10:08:33 +0530

    Add partitionString method to partition input string
    Implement partitionString method to partition a string into substrings with unique characters.

---
+++ LeetCode /partition-string.py
@@ -0,0 +1,21 @@
+/*
+Example 1:
+
+Input: s = "abbccccd"
+
+Output: ["a","b","bc","c","cc","d"]
+*/
+
+
+class Solution:
+    def partitionString(self, s: str) -> List[str]:
+        vis = set()
+        ans = []
+        t = ""
+        for c in s:
+            t += c
+            if t not in vis:
+                vis.add(t)
+                ans.append(t)
+                t = ""
+        return ans
