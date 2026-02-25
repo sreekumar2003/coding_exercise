ab30c86bd4bacc08e08f48bd50dd46f646871ce0
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2026-02-25 23:00:27 +0530

    Add searchInsert function for position search
    Implement searchInsert method to find target index.

---
+++ LeetCode /Search Insert Position.py
@@ -0,0 +1,26 @@
+/*
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4  
+*/
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            for i in range(len(nums)):
+                if target<nums[i]:
+                    return nums.index(nums[i])
+
+        return len(nums)          
+        
