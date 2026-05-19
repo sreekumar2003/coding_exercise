1fc1f6b9638c279ec864261b22dbfb5384e3598a
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2026-05-19 13:54:31 +0530

    Add examples for Count Symmetric Integer
    Added examples for the Count Symmetric Integer function.

--- LeetCode /Count Symmetric Integer.py
+++ LeetCode /Count Symmetric Integer.py
@@ -14,4 +14,15 @@ class Solution:
                 c+=1
         return c
 
+Example 1:
+
+Input: low = 1, high = 100
+Output: 9
+Explanation: There are 9 symmetric integers between 1 and 100: 11, 22, 33, 44, 55, 66, 77, 88, and 99.
+Example 2:
+
+Input: low = 1200, high = 1230
+Output: 4
+Explanation: There are 4 symmetric integers between 1200 and 1230: 1203, 1212, 1221, and 1230.
+
 
