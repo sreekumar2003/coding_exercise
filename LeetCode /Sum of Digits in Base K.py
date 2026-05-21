6950640ef06e89f6e890e49130a975e9a91edc7c
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2026-05-21 12:24:02 +0530

    Add examples for sum of digits in base K function
    Added examples for the function that calculates the sum of digits in base K.

--- LeetCode /Sum of Digits in Base K.py
+++ LeetCode /Sum of Digits in Base K.py
@@ -7,3 +7,13 @@ class Solution:
             su += s
         return su
         
+Example 1:
+
+Input: n = 34, k = 6
+Output: 9
+Explanation: 34 (base 10) expressed in base 6 is 54. 5 + 4 = 9.
+Example 2:
+
+Input: n = 10, k = 10
+Output: 1
+Explanation: n is already in base 10. 1 + 0 = 1.
