8902a5207b0bcae0cfdec27eff6cd0bbb3f16229
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2026-05-21 12:23:13 +0530

    Add solution for sum of digits in base K

---
+++ LeetCode /Sum of Digits in Base K.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        su = 0
+        while n>0:
+            s = n%k
+            n = n//k
+            su += s
+        return su
+        
