dad72d9c2750cb5683b001654f5c099ec80c44b7
SREEKUMAR A <140206906+sreekumar2003@users.noreply.github.com>
2026-06-10 11:10:50 +0530

    Add examples for getCommon function in Minimum Common Value
    Added examples and explanations for the getCommon function.

--- LeetCode /Minimum Common Value.py
+++ LeetCode /Minimum Common Value.py
@@ -3,3 +3,13 @@ class Solution:
         if (set(nums2).intersection(set(nums1))):
             return min(set(nums2).intersection(set(nums1)))
         return -1
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return 2.
+Example 2:
+
+Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+Output: 2
+Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so 2 is returned.
